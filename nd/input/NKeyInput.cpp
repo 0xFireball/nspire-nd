e2fd81e10ac9a05e3c7f7f4c98be2b551a9ab97d
0xFireball <0xFireball@outlook.com>
2017-08-16 23:42:32 -0700

    support key polling by keycode

--- nd/input/NKeyInput.cpp
+++ nd/input/NKeyInput.cpp
@@ -2,15 +2,26 @@
 #include "NKeyInput.h"
 
 void NKeyInput::pump_keydown(int k) {
-    if (this->_state.count(k) == 0) { this->fill_position(k); }
+    this->ensure_position(k);
     this->_state[k].pump_press();
 }
 
 void NKeyInput::pump_keyup(int k) {
-    if (this->_state.count(k) == 0) { this->fill_position(k); }
+    this->ensure_position(k);
     this->_state[k].pump_release();
+}
+
+void NKeyInput::ensure_position(int k) {
+    if (this->_state.count(k) == 0) { this->fill_position(k); }
 }
 
 void NKeyInput::fill_position(int k) {
     this->_state[k] = NKeyState();
 }
+
+bool NKeyInput::pressed(int key) {
+    this->ensure_position(k);
+    NKeyState keyState = this->state[key];
+    return keyState.pressed();
+}
+
--- nd/input/NKeyInput.h
+++ nd/input/NKeyInput.h
@@ -10,8 +10,11 @@ class NKeyInput {
     std::map<int, NKeyState> _state;
 
     void fill_position(int k);
+    void ensure_position(int k);
 
   public:
     void pump_keydown(int k);
     void pump_keyup(int k);
+
+    bool pressed(int key);
 };
