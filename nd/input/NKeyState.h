d54b0544a661a1e51613a964821ffb48f20b16c3
0xFireball <0xFireball@outlook.com>
2017-08-19 12:07:51 -0700

    fix justPressed

--- nd/input/NKeyState.h
+++ nd/input/NKeyState.h
@@ -3,24 +3,20 @@
 
 #include "../system/NGame.h"
 
-#define JUST_PRESSED_FRAMES 1
-
 class NKeyState {
   private:
     bool _down = false;
     int _pressFrame = 0;
-    bool _justDown = false;
 
   public:
     void pump_press(int frame = 0) {
         _down = true;
         _pressFrame = frame;
-        _justDown = false;
     }
 
     void pump_release() {
         _down = false;
-        _justDown = false;
+        _pressFrame = 0;
     }
 
     bool pressed() {
@@ -28,7 +24,6 @@ class NKeyState {
     }
 
     bool justPressed(int frame) {
-        _justDown = pressed() && !_justDown && (frame - _pressFrame) <= JUST_PRESSED_FRAMES;
-        return _justDown;
+        return pressed() && frame == _pressFrame;
     }
 };
