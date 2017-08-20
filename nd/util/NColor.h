04252eeb158d19c68289a46d62af508f631c0856
0xFireball <0xFireball@outlook.com>
2017-08-19 18:13:52 -0700

    add randCol

--- nd/util/NColor.h
+++ nd/util/NColor.h
@@ -1,6 +1,8 @@
 
 #pragma once
 
+#include "rng.h"
+
 #define COL_FLOAT_CONV 255.999
 
 struct NColor {
@@ -16,4 +18,9 @@ struct NColor {
     }
 
     int fmt(float v) { return v * COL_FLOAT_CONV; }
+
+    static NColor rand_col(float r, float g, float b, float variation = 0.1) {
+        float colDiff = RNG::randf() * (variation * 2) - variation;
+        return NColor(r + colDiff, g + colDiff, b + colDiff);
+    }
 };
