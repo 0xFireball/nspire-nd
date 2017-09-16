64bc313ad433bdf6c90925634126cac1accf26c6
0xFireball <0xFireball@outlook.com>
2017-09-16 16:41:25 -0700

    forgot to explicitly initialize immovable

--- nd/entity/NEntity.h
+++ nd/entity/NEntity.h
@@ -15,7 +15,7 @@ public:
   float y;
   float width;
   float height;
-  bool immovable;
+  bool immovable = false;
 
   NEntity(float x = 0, float y = 0, float width = 0, float height = 0)
       : NBasic(), x(x), y(y), width(width), height(height) {}
